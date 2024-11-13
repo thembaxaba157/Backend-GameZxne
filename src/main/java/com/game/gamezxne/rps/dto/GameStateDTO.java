78a3620bed915cfa0678caa70431ecea99d2cb60
thembaxaba157 <thembaxaba157@gmail.com>
2024-11-13 09:02:55 +0200

    dto

--- src/main/java/com/game/gamezxne/rps/dto/GameStateDTO.java
+++ src/main/java/com/game/gamezxne/rps/dto/GameStateDTO.java
@@ -1,5 +1,14 @@
 package com.game.gamezxne.rps.dto;
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class GameStateDTO {
-    
+    private Long id;
+    private String lobbyName;
+    private List<PlayerStateDTO> currentPlayers;
+    private String lobbyOwner;
+
 }
---
+++ src/main/java/com/game/gamezxne/rps/dto/PlayerStateDTO.java
@@ -0,0 +1,13 @@
+package com.game.gamezxne.rps.dto;
+
+import lombok.Data;
+
+@Data
+public class PlayerStateDTO {
+
+    private String username;
+    private Long id;
+    private int score;
+
+
+}
